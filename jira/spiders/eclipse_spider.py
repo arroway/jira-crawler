660f239da76b16080e2686186a4a820093473ef2
arroway <stephanie@minet.net>
2013-06-13 17:12:03 +0200

    add spider to get eclipse project name

---
+++ jira/spiders/eclipse_spider.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# encoding=utf-8
+ 
+from scrapy.spider import BaseSpider
+from scrapy.http import Request
+from scrapy.http import FormRequest
+from scrapy.selector import HtmlXPathSelector
+from scrapy import log
+import sys
+import re
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+class EclipseSpider(BaseSpider):
+  name = "eclipse"
+  allowed_domains = ["bugs.eclipse.org"]
+  start_urls = ["https://bugs.eclipse.org/bugs/describecomponents.cgi"]
+
+  def parse(self, response):
+    hxs = HtmlXPathSelector(response)
+    file_eclipse = open("./eclipseProjects.txt", 'w')
+ 
+    for i in range(1, 200):
+
+      # Check the item exist or else we are out of bounds
+      div = '/html/body/div[2]/table/tr[' + str(i) + ']/th/a'  
+      getdiv = hxs.select(div).extract()
+
+      if getdiv:
+
+        # Crapy regexps but it works
+        n = re.match(r".*\">(\w+)</a>.*", str(getdiv))
+
+        if n:
+          file_eclipse.write(str(n.group(1)) + '|')
+
+      else:
+        break
